d085a0c22847ccd7492f53d055f0ef071e9a1dc3
Matthew Hammer <matthew.hammer@colorado.edu>
2016-12-24 10:58:18 -0700

    updated docs

---
+++ rustdoc/implementors/serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["adapton"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/serialize/serialize/trait.Encodable.html' title='serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='adapton/engine/struct.Cnt.html' title='adapton::engine::Cnt'>Cnt</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
