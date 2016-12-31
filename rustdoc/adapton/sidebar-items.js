74183ce406036b47d5a7e436d549b369d988d17b
Matthew Hammer <matthew.hammer@colorado.edu>
2016-12-31 11:11:32 -0700

    updated rustdoc

--- rustdoc/adapton/sidebar-items.js
+++ rustdoc/adapton/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["cell_call",""],["eager",""],["memo",""],["prog_pt",""],["thunk",""]],"mod":[["arith_parse",""],["bitstring",""],["collections",""],["engine",""],["fixpoints",""],["macros",""],["trie",""]]});
+initSidebarItems({"macro":[["cell_call",""],["eager",""],["memo",""],["prog_pt",""],["thunk",""]],"mod":[["arith_parse",""],["bitstring",""],["collections",""],["engine",""],["fixpoints",""],["macros",""],["reflect",""],["trie",""]]});
